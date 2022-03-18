62d31682b6754356000685d96331f73f4460ed30
Myth <lxbpxylps@126.com>
2022-03-18 18:58:19 +0800

    feat: add cxxopts library

--- Latte/main.cpp
+++ Latte/main.cpp
@@ -1,12 +1,28 @@
+#include "cxxopts/cxxopts.hpp"
 #include "easyloggingpp/easylogging++.h"
 #include "camera/camera.h"
 #include "rockx_face/rockx_face.h"
 
 INITIALIZE_EASYLOGGINGPP // NOLINT
 
-int main()
+int main(int argc, char **argv)
 {
-    Camera camera("/dev/v4l/by-id/usb-Generic_HD_camera-video-index0");
+    cxxopts::Options options("Latte", "Coffee machine system on TB-RK3399ProD.");
+    options.allow_unrecognised_options();
+    options.add_options()
+            ("c,camera", "Camera device path",
+             cxxopts::value<std::string>()->default_value("/dev/v4l/by-id/usb-Generic_HD_camera-video-index0"))
+            ("h,help", "Help");
+
+    auto result = options.parse(argc, argv);
+
+    if (result.count("help"))
+    {
+        std::cout << options.help() << std::endl;
+        return 0;
+    }
+
+    Camera camera(result["camera"].as<std::string>().c_str());
     RockxFace rockx_face;
 
     cv::Mat frame;
