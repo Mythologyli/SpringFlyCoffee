7f9dcd14310e04ccf4a065cf5065fe81ebc876b1
Myth <lxbpxylps@126.com>
2022-03-21 15:06:30 +0800

    feat: print received message

--- Cappuccino/main.py
+++ Cappuccino/main.py
@@ -4,8 +4,8 @@ import websockets
 
 async def echo(websocket, path):
     async for message in websocket:
-        message = 'check'
-        await websocket.send(message)
+        print(message)
+        await websocket.send('check')
 
 
 asyncio.get_event_loop().run_until_complete(websockets.serve(echo, '0.0.0.0', 9876))
