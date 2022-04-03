ccdd34126e6d2235908de89bd1f510f918a956bf
Myth <lxbpxylps@126.com>
2022-04-04 01:08:36 +0800

    feat: controlled by serial command

--- Espresso/Espresso.ino
+++ Espresso/Espresso.ino
@@ -9,8 +9,6 @@ Pump pump1(2, 3);
 Pump pump2(4, 5);
 HcSr04 hcsr04(6, 7);
 
-float distance = 0.0;
-
 void setup()
 {
     pump1.init();
@@ -23,11 +21,59 @@ void setup()
 
 void loop()
 {
-    float currentDistance = hcsr04.getDistance();
-    if (distance != currentDistance)
+    char ch = Serial.read();
+    Pump *pump = nullptr;
+
+    if (ch == '1')
     {
-        distance = currentDistance;
-        Serial.print("Distance: ");
-        Serial.println(distance);
+        pump = &pump1;
     }
+    else if (ch == '2')
+    {
+        pump = &pump2;
+    }
+    else
+    {
+        return;
+    }
+
+    unsigned long startTime = 0;
+    unsigned long oldTotalOpenedTime = 0;
+    unsigned long totalOpenedTime = 0;
+    bool isOpened = false;
+
+    while (true)
+    {
+        if (isOpened)
+        {
+            if (hcsr04.getDistance() < 4.0)
+            {
+
+                totalOpenedTime = millis() - startTime + oldTotalOpenedTime;
+            }
+            else
+            {
+                Serial.println('2');
+                pump->stop();
+                isOpened = false;
+            }
+        }
+        else if (hcsr04.getDistance() < 4.0)
+        {
+            Serial.println('1');
+            pump->start();
+            isOpened = true;
+            startTime = millis();
+            oldTotalOpenedTime = totalOpenedTime;
+        }
+
+        ch = Serial.read();
+        if (ch == '0' || totalOpenedTime >= 10000)
+        {
+            pump->stop();
+            break;
+        }
+    }
+
+    Serial.println('0');
 }
